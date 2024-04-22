9228385bd6c659da0e2d6e301bc347e19454f3d3
Sajjad fani <faniam321@gmail.com>
2024-04-22 23:08:04 +0330

    complete urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from .views import (
+    Login, Register, ChangePassword, RetrieveUpdateDestroyAccount,
+)
 
 app_name = "users"
 
 urlpatterns = [
+    path("login/", Login.as_view(), name="Login"),
+    path("register/", Register.as_view(), name="Register"),
 
+    path("password/change/", ChangePassword.as_view(), name="ChangePassword"),
+
+    path("update/<int:pk>/",
+         RetrieveUpdateDestroyAccount.as_view(), name="RetrieveUpdateDestroyAccount"),
 ]
