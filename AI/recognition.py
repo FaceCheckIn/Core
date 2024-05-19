4d756e1a41b3cbac58eb8288c336c299f50f900c
Sajjad fani <faniam321@gmail.com>
2024-05-19 22:04:14 +0330

    refactor code

--- AI/recognition.py
+++ AI/recognition.py
@@ -2,9 +2,9 @@ import face_recognition
 import numpy as np
 
 
-def encode_known_faces(image_paths, names):
+def encode_known_faces(image_paths, identification_codes):
     employees_face_encodings, employees_face_names = [], []
-    for image_path, name in zip(image_paths, names):
+    for image_path, name in zip(image_paths, identification_codes):
         known_image = face_recognition.load_image_file(image_path)
         face_locations = face_recognition.face_locations(known_image)
         if len(face_locations) > 0:
@@ -15,9 +15,9 @@ def encode_known_faces(image_paths, names):
     return employees_face_encodings, employees_face_names
 
 
-def recognition(input_image, employees_faces, employees_names):
+def recognition(input_image, employees_faces, employees_identification_codes):
     employees_face_encodings, employees_face_names = encode_known_faces(
-        employees_faces, employees_names)
+        employees_faces, employees_identification_codes)
     matches = face_recognition.compare_faces(
         employees_face_encodings, input_image)
     faceDis = face_recognition.face_distance(
