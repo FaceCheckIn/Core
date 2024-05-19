6e7f305103a853f6473ebac07dacdfc33a653705
Sajjad fani <faniam321@gmail.com>
2024-05-19 22:01:55 +0330

    refactor code

--- AI/recognition.py
+++ AI/recognition.py
@@ -1,38 +1,29 @@
-import cv2
 import face_recognition
 import numpy as np
-import time
-
-
-emloyees_face_encodings = []
-emloyees_face_names = []
 
 
 def encode_known_faces(image_paths, names):
+    employees_face_encodings, employees_face_names = [], []
     for image_path, name in zip(image_paths, names):
         known_image = face_recognition.load_image_file(image_path)
         face_locations = face_recognition.face_locations(known_image)
         if len(face_locations) > 0:
             face_encodings = face_recognition.face_encodings(
                 known_image, face_locations)
-            emloyees_face_encodings.extend(face_encodings)
-            emloyees_face_names.extend([name] * len(face_encodings))
+            employees_face_encodings.extend(face_encodings)
+            employees_face_names.extend([name] * len(face_encodings))
+    return employees_face_encodings, employees_face_names
 
 
-def recognition(detected_face, employees_faces):
-    # load face and names from data base
-    # employees_face = ["images.jpg", "images 3.jpg",
-    #                   "newme.jpeg", "Dr.Eskandari.jpg"]
-    # employees_name = ["CR7", "Messi", "Mostafa", "DrEskandari"]
-
-    encode_known_faces(employees_face, employees_name)
+def recognition(input_image, employees_faces, employees_names):
+    employees_face_encodings, employees_face_names = encode_known_faces(
+        employees_faces, employees_names)
     matches = face_recognition.compare_faces(
-        emloyees_face_encodings, detected_face)
+        employees_face_encodings, input_image)
     faceDis = face_recognition.face_distance(
-        emloyees_face_encodings, detected_face)
+        employees_face_encodings, input_image)
     name = "unknown"
     if True in matches:
         match_index = np.argmin(faceDis)
-        name = emloyees_face_names[match_index]
-
+        name = employees_face_names[match_index]
     return True, name
