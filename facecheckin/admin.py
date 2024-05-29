061083bc2580fff668d1fc6daef0e12714fed2be
Sajjad fani <faniam321@gmail.com>
2024-05-30 02:36:38 +0330

    complete admin

--- facecheckin/admin.py
+++ facecheckin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sentiment', 'status', 'datetime')
+    readonly_fields = ('user', 'sentiment', 'status', 'datetime')
+
+
+admin.site.register(Transaction, TransactionAdmin)
